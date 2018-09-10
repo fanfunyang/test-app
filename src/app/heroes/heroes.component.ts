34db648eff83bd8eaa23fd5c3fc286309ab0f8ea
fanfunyang <1011232866@qq.com>
2018-09-10 16:25:04 +0800

    第三次修改

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero = '老大';
-  constructor() { }
-
+  heroes: Hero[];
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  constructor(private heroService: HeroService) {
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] =[
+  {id: 11, name: '亚索'},
+  {id: 12, name: '劫'},
+  {id: 13, name: '李青'},
+  {id: 14, name: '内瑟斯'},
+  {id: 15, name: '马尔扎哈'},
+  {id: 16, name: '盖伦'},
+  {id: 17, name: '随便'},
+  {id: 18, name: '牛头'},
+  {id: 19, name: '人马'},
+  {id: 20, name: '动物园'},
+]
